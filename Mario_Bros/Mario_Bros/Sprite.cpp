dda81e56cd40291d617f5d94d2492cb0db64eabd
Mohsen <renegade@Mohsens-MacBook-Pro.local>
2018-11-14 16:29:28 -0600

    Added Sprite Class

---
+++ Mario_Bros/Mario_Bros/Sprite.cpp
@@ -0,0 +1,9 @@
+//
+//  Sprite.cpp
+//  Mario_Bros
+//
+//  Created by Mohsen on 11/14/18.
+//  Copyright © 2018 [Insert Name Here]. All rights reserved.
+//
+
+#include "Sprite.hpp"
---
+++ Mario_Bros/Mario_Bros/Sprite.hpp
@@ -0,0 +1,14 @@
+//
+//  Sprite.hpp
+//  Mario_Bros
+//
+//  Created by Mohsen on 11/14/18.
+//  Copyright © 2018 [Insert Name Here]. All rights reserved.
+//
+
+#ifndef Sprite_hpp
+#define Sprite_hpp
+
+#include <stdio.h>
+
+#endif /* Sprite_hpp */
